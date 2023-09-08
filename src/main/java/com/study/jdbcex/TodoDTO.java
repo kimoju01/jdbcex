46337f27dcabd4682a80a2199788c5e1a8822ddf
Hyeju <kimoju03@naver.com>
2023-09-08 17:50:33 +0900

    Day 5
    - log4j2 라이브러리 추가
    - https://www.notion.so/Log4j2-8211d9e5a9ba459e89093dc18b4ddac9?pvs=4

--- src/main/java/com/study/jdbcex/TodoDTO.java
+++ src/main/java/com/study/jdbcex/TodoDTO.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Builder
-@Data  // getter, setter, toString, equals, hashCode 등을 모두 컴파일할 때 생성해준다.
-// @AllArgs.., @NoArgs.. 파라미터가 없는 생성자와 모든 필드값이 필요한 생성자를 만든다.
+@Data  // getter, setter, toString, equals, hashCode, requiredArgsConstructor를 모두 컴파일할 때 생성해준다.
+// @AllArgs.., @NoArgs.. 파라미터가 없는 기본 생성자와 모든 필드값이 필요한 생성자를 만든다.
 @NoArgsConstructor
 @AllArgsConstructor
 // TodoVO와 완전히 같은 구조이지만 어노테이션이 다르다. VO는 setter 없이 주로 읽기 전용 작업을 위해 사용한다.
